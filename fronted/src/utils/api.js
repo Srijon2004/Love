a0bd63ee879c999993c26d4f674b897ba3ce7a13
Srijon2004 <srijonchowdhury6@gmail.com>
2025-08-30 23:53:34 +0530

    Deploy

--- fronted/src/utils/api.js
+++ fronted/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://love-backend-nsbo.onrender.com", // ✅ matches your backend
+  baseURL: "https://love-backend-nsbo.onrender.com/api", // ✅ matches your backend
   withCredentials: true, 
 });
 
